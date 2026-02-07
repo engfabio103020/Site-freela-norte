e8439181b91a5f1524aa232e80945b21ff6e4230
Fábio Magalhães <103157748+engfabio103020@users.noreply.github.com>
2026-02-07 15:28:17 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const mainNav = document.querySelector(".main-nav");
+
+menuToggle?.addEventListener("click", () => {
+  mainNav?.classList.toggle("is-open");
+});
+
+mainNav?.querySelectorAll("a").forEach((item) => {
+  item.addEventListener("click", () => {
+    mainNav?.classList.remove("is-open");
+  });
+});
